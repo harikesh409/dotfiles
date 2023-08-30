243420847bbafa3aa075fec45bc3a887e79a8389
Harikesh Pallantla <p.harikesh409@gmail.com>
2023-08-30 16:54:00 +0530

    Add applications

--- install/brew.sh
+++ install/brew.sh
@@ -1,5 +1,91 @@
 # Install Homebrew
 export HOMEBREW_NO_INSTALL_FROM_API=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Make sure we’re using the latest Homebrew.
 brew update
+
+# Upgrade any already-installed formulae.
 brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+brew install 1password-cli
+brew install adobe-acrobat-reader
+brew install adoptopenjdk11
+brew install adoptopenjdk8
+brew install appcleaner
+brew install awscli
+brew install azure-cli
+brew install azure-data-studio
+brew install brave-browser
+brew install cakebrew
+brew install colima
+brew install dbeaver-community
+brew install docker
+brew install docker-completion
+brew install docker-compose
+brew install docker-credential-helper
+brew install eul
+brew install fd
+brew install fliqlo
+brew install fluxctl
+brew install fzf
+brew install gh
+brew install gimp
+brew install git
+brew install git-credential-manager-core
+brew install git-extras
+brew install github
+brew install google-chrome
+brew install helm
+brew install helmfile
+brew install intellij-idea
+brew install iterm2
+brew install itsycal
+brew install jenv
+brew install jpeg
+brew install jpeg-turbo
+brew install jq
+brew install k9s
+brew install keka
+brew install keybase
+brew install kubectx
+brew install kubernetes-cli
+brew install libreoffice
+brew install maven
+brew install neovim
+brew install node
+brew install notion
+brew install nvm
+brew install openjdk@17
+brew install openssl@1.1
+brew install postman
+brew install pre-commit
+brew install shottr
+brew install slack
+brew install soapui
+brew install sops
+brew install stern
+brew install sublime-text
+brew install telepresence
+brew install tmux
+brew install tree
+brew install visual-studio-code
+brew install vlc
+brew install yq
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
