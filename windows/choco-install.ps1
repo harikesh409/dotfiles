3ca739cb22828a4a3ac70e6506d889da7d309ab5
Harikesh Pallantla <p.harikesh409@gmail.com>
2024-05-03 12:52:08 +0530

    Create choco-install.ps1

---
+++ windows/choco-install.ps1
@@ -0,0 +1,2 @@
+#Run as admin
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
