4354cfe45b85c8dc45cee501efec34df2489ec48
Harikesh Pallantla <p.harikesh409@gmail.com>
2024-05-03 12:48:51 +0530

    Create Microsoft.PowerShell_profile.ps1

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/harikesh409/dotfiles/master/oh-my-posh/hari.omp.json' | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Fzf
+# https://github.com/kelleyma49/PSFzf?tab=readme-ov-file#reverse-search-through-psreadline-history-default-chord-ctrlr
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
