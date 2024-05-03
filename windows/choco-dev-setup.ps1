8149e57e0ad24c2928f0cba7d7d9b5ac7b7fa313
Harikesh Pallantla <p.harikesh409@gmail.com>
2024-05-03 12:51:25 +0530

    Create choco-dev-setup.ps1

---
+++ windows/choco-dev-setup.ps1
@@ -0,0 +1,29 @@
+ECHO Installing apps
+
+ECHO Configure chocolatey
+choco feature enable -n allowGlobalConfirmation
+
+choco install chocolateygui
+
+choco install googlechrome
+choco install brave
+
+choco install nodejs.install
+choco install jdk8
+choco install python3
+choco install maven
+
+choco install vscode.install
+choco install intellijidea-community
+choco install eclipse
+choco install notepadplusplus.install
+choco install git.install
+choco install postman
+choco install soapui
+
+choco install ccleaner
+choco install zoom
+choco install 7zip.install
+choco install vlc
+
+choco feature disable -n allowGlobalConfirmation
